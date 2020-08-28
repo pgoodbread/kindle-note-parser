ca58177b03fab7be55dc791d326a0e4675c6662d
Philip Gutbrodt <philip.gutbrodt@spring-media.de>
2020-08-28 14:29:15 +0200

    move extraction of title and author to functions

--- bin/kindle-note-parser.js
+++ bin/kindle-note-parser.js
@@ -19,52 +19,97 @@ const titles = [];
 
 const processedHighlights = {};
 
-// go through each highlight
+// go through each highlight, and curate authors and titles
 highlights.map((note) => {
   let lines = note.split("\n");
 
   let titleAuthorLine = lines[0].trim();
 
-  //create unique hash
+  //create unique hash for book title and author
   let shasum = crypto.createHash("sha1");
   shasum.update(titleAuthorLine);
 
   let titleAuthorHash = shasum.digest("hex");
 
+  // if hash already exists, skip this highlight
+  if(processedHighlights.hasOwnProperty(titleAuthorHash)) {
+    return
+  } 
+
   processedHighlights[titleAuthorHash] = {};
-
-  // ----- AUTHOR -----
-  // match for bracket content at end of the line
-  let authorMatches = titleAuthorLine.match(/\(([\w\s,\. ]+)\)$/);
-
-  // If there are matches, add the author 
-  if(authorMatches !== null && authorMatches.length > 0) {
-
-    // The Author name without brackets is found in the first capture group
-    let author = authorMatches[1]
-
-    // Add Author, if not yet added and if it doesnt contain "Edition", because then it's not really an author
-    if (!authors.includes(author) && !author.toLowerCase().includes('edition')) {
-      authors.push(author);
-    }
-
-  }
-
-  // ----- TITLES -----
-  // match everything that is followed by what has been identified as author
-  let titleMatches = lines[0].match(/.+(?=\([\w\s,. ]+\)$)/)
-
-  if(titleMatches !== null && titleMatches.length > 0) {
-    let title = titleMatches[0].trim()
-    if(!titles.includes(title)) {
-      titles.push(title)
-    }
-  }
+  processedHighlights[titleAuthorHash]['author'] = extractAuthor(titleAuthorLine);
+  processedHighlights[titleAuthorHash]['title'] = extractTitle(titleAuthorLine);
+  processedHighlights[titleAuthorHash]['notes'] = [];
 
 })
 
 
-titles.map((el) => {
-  console.log(el)
-  console.log("-------------")
-});
+
+//go through each note and add note data to correct hash (title and author)
+highlights.map((note) => {
+
+  let lines = note.split("\n");
+
+
+  // ----- NOTES -----
+  let noteData = {}
+  let positionDateLine = lines[1]
+
+  // --- PAGE ---
+  // check if a page is given
+  let hasPageInfo = positionDateLine.match(/\|/g).length > 1
+  let page = 'unknown';
+
+  if(hasPageInfo) {
+    let page = positionDateLine.match(/\d+/g)[0]
+    noteData.page = page
+  }
+
+  // --- POSITION ---
+  let positionInfo = positionDateLine.match(/\d+-\d+/)[0].split('-');
+  noteData.startPosition = positionInfo[0]
+  noteData.endPosition = positionInfo[1]
+
+  processedHighlights[titleAuthorHash]["notes"] = noteData
+
+})
+
+
+console.log('processedHighlights', processedHighlights)
+
+// ----- TITLES -----
+function extractTitle(line) {
+
+  let title = 'unknown'
+
+
+  // match everything that is followed by what has been identified as author
+  let titleMatches = line.match(/.+(?=\([\w\s,. ]+\)$)/)
+
+  // if title match exists, set title
+  if (titleMatches !== null && titleMatches.length > 0) {
+    title = titleMatches[0].trim()
+  }
+
+  return title
+}
+
+// ----- AUTHOR -----
+function extractAuthor(line) {
+
+  let author = 'unknown';
+
+  // match for bracket content at end of the line
+  let authorMatches = line.match(/\(([\w\s,\. ]+)\)$/)
+
+  // If there are matches and the author doesn't contain "edition", set the author 
+  if (
+    authorMatches !== null &&
+    authorMatches.length > 0 &&
+    !authorMatches[1].toLowerCase().includes("edition")
+  ) {
+      // The Author name without brackets is found in the first capture group
+      author = authorMatches[1];
+  }
+  return author
+}
